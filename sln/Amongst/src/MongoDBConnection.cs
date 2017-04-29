e7abcd1e053a630ae00580054f95487053b2378e
Steven <steven.wobser@gmail.com>
2017-04-29 15:49:51 +0200

    Added connection class
    CLR to keywords
    Keeping track of ports in use

---
+++ sln/Amongst/src/MongoDBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Amongst
+{
+    internal class MongoDBConnection
+    {
+        public IPAddress IP { get; }
+        public short Port { get; }
+
+        public MongoDBConnection(IPAddress ip, short port)
+        {
+            IP = ip;
+            Port = port;
+        }
+
+        public override string ToString()
+        {
+            return $"mongodb://{IP}:{Port}";
+        }
+    }
+}
--- sln/Amongst/src/MongoDBInstance.cs
+++ sln/Amongst/src/MongoDBInstance.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -15,44 +16,28 @@ namespace Amongst
 {
     public class MongoDBInstance : IDisposable
     {
+        private static ConcurrentBag<int> _portsInUse = new ConcurrentBag<int>();
+
         private const string BIN_WIN32 = "mongodb-win32-x86_64-2008plus-3.4.4";
         private const string BIN_LINUX = "mongodb-linux-x86_64-3.4.4";
         private const string BIN_OSX = "mongodb-osx-x86_64-3.4.4";
 
         private readonly IMongoDBInstanceOutputHelper _output;
-        private static Int32 _instanceCount;
+        private static int _instanceCount;
 
+        private readonly MongoDBConnection _connection;
         private readonly Process _process;
         private readonly ManualResetEventSlim _manualReset;
 
         private Mutex _mutex;
 
         public Guid Id;
-        public string Connectionstring { get; }
+        public string ConnectionString => _connection.ToString();
         public MongoDBInstaceState State { get; private set; }
 
         public static MongoDBInstance Spawn(IMongoDBInstanceOutputHelper output = null)
         {
             return new MongoDBInstance(output);
-        }
-
-        private void AssertSingleInstance()
-        {
-            const string moduleGuid = "9A66C037-5217-4125-82F8-DBB7C6E415AB";
-
-            _mutex = new Mutex(false, $"Global\\{moduleGuid}");
-            if (!_mutex.WaitOne(0)) {
-                _mutex.Dispose();
-
-                throw new MultipleRunnerInstancesException("Multiple Runner instances detected.");
-            }
-
-            if (_instanceCount > 1)
-                _output.WriteLine(
-                    $"[Warning]: You already spawned {_instanceCount} instances of {nameof(MongoDBInstance)}. " +
-                    "It is recommended to share a MongoDB instance across your tests using a fixture. " +
-                    "You can read more about shared context wihin xUnit here: https://xunit.github.io/docs/shared-context.html " +
-                    "If this is intentional, you can igore this Warning.");
         }
 
         private MongoDBInstance(IMongoDBInstanceOutputHelper output)
@@ -77,24 +62,23 @@ namespace Amongst
 
             AssertSingleInstance();
 
-            const string fileName = "mongod";
-            const string bind = "127.0.0.1";
+            _connection = new MongoDBConnection(
+                IPAddress.Loopback,
+                GetAvailablePort()
+            );
 
-            var port = GetAvailablePort();
             var args = new[]
             {
-                $"--bind_ip {bind}",
-                $"--port {port}",
+                $"--bind_ip {_connection.IP}",
+                $"--port {_connection.Port}",
                 $"--dbpath {dbPathUri}",
                 "--noprealloc",
                 "--smallfiles",
                 "--nojournal"
             };
 
-            var connectionstring = $"mongodb://{bind}:{port}/";
-
             var binaryPath = GetBinaryPath();
-            var fullPath = Path.Combine(binaryPath, fileName);
+            var fullPath = Path.Combine(binaryPath, "mongod");
 
             _process = new Process
             {
@@ -126,12 +110,30 @@ namespace Amongst
             if (!_manualReset.IsSet)
                 throw new MongodStartupTimeoutException("mongod failed to start after 30 seconds.");
 
-            Connectionstring = connectionstring;
-
             _instanceCount++;
         }
 
-        private void FetchReadyState(Object sender, DataReceivedEventArgs e)
+        private void AssertSingleInstance()
+        {
+            const string moduleGuid = "9A66C037-5217-4125-82F8-DBB7C6E415AB";
+
+            _mutex = new Mutex(false, $"Global\\{moduleGuid}");
+            if (!_mutex.WaitOne(0))
+            {
+                _mutex.Dispose();
+
+                throw new MultipleRunnerInstancesException("Multiple Runner instances detected.");
+            }
+
+            if (_instanceCount > 1)
+                _output.WriteLine(
+                    $"[Warning]: You already spawned {_instanceCount} instances of {nameof(MongoDBInstance)}. " +
+                    "It is recommended to share a MongoDB instance across your tests using a fixture. " +
+                    "You can read more about shared context within xUnit here: https://xunit.github.io/docs/shared-context.html " +
+                    "If it is intentional, you can igore this Warning.");
+        }
+
+        private void FetchReadyState(object sender, DataReceivedEventArgs e)
         {
             const string pattern = "waiting for connections on port";
 
@@ -144,7 +146,7 @@ namespace Amongst
             p.OutputDataReceived -= FetchReadyState;
         }
 
-        private void OnOutputDataReceived(Object sender, DataReceivedEventArgs e)
+        private void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             var p = sender as Process;
             if (p == null || string.IsNullOrEmpty(e.Data)) return;
@@ -155,7 +157,7 @@ namespace Amongst
             _output.WriteLine($"[{DateTime.Now}][Info][{name}:{id}][{Id:N}]: {e.Data}");
         }
 
-        private void OnErrorDataReceived(Object sender, DataReceivedEventArgs e)
+        private void OnErrorDataReceived(object sender, DataReceivedEventArgs e)
         {
             var p = sender as Process;
             if (p == null || string.IsNullOrEmpty(e.Data)) return;
@@ -166,9 +168,10 @@ namespace Amongst
             _output.WriteLine($"[{DateTime.Now}][Error][{name}:{id}][{Id:N}]: {e.Data}");
         }
 
-        private void OnExited(Object sender, EventArgs e)
+        private void OnExited(object sender, EventArgs e)
         {
             State = MongoDBInstaceState.Stopped;
+            _portsInUse.Take(_connection.Port);
 
             var p = sender as Process;
             if (p == null) return;
@@ -220,11 +223,12 @@ namespace Amongst
             return Path.Combine(GetBasePath(), "tools", version, "bin");
         }
 
-        private static Int16 GetAvailablePort()
+        private static short GetAvailablePort()
         {
-            const Int16 begin = 27018;
+            const short begin = 27018;
 
-            var port = (Int16) Enumerable.Range(begin, Int16.MaxValue)
+            var port = (short) Enumerable.Range(begin, short.MaxValue)
+                .Except(_portsInUse)
                 .FirstOrDefault(p =>
                 {
                     var listener = new TcpListener(IPAddress.Loopback, p);
@@ -244,6 +248,8 @@ namespace Amongst
             if (port < begin)
                 throw new NoPortAvailableException(
                     "Counld not create a new mongodb instance. No port available.");
+
+            _portsInUse.Add(port);
 
             return port;
         }
