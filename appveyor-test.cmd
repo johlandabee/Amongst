3b2a52dd0612fbd49e5bf802a56b03f51c76f748
Steven <steven.wobser@gmail.com>
2017-05-22 13:10:56 +0200

    Well almost

--- appveyor-test.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set CI=True
-set CONFIGURATION=Release
-set LABEL=Test
-
-dotnet restore -v Minimal
-dotnet build -c %CONFIGURATION% --no-dependencies --version-suffix adssada
-dotnet test "sln\Amongst.Test\Amongst.Test.csproj" --no-build -c %CONFIGURATION%
