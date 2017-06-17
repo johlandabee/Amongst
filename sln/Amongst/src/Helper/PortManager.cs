a3f956826cc30162a34b82c1811c9dd1b83a109c
Steven <steven.wobser@gmail.com>
2017-06-18 01:50:47 +0200

    WIP: Splitted MongoDBInstance.cs into multiple files
    Extracted port related code into PortManager.cs
    Some shenanigans

---
+++ sln/Amongst/src/Helper/PortManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using Amongst.Exception;
+
+namespace Amongst.Helper
+{
+    public class PortManager
+    {
+        private static readonly object Sync = new object();
+        private static readonly List<int> PortsInUse = new List<int>();
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        public static short GetAvailablePort()
+        {
+            const short begin = 27018;
+            const short count = 100;
+
+            var port = (short) Enumerable.Range(begin, count)
+                .Except(PortsInUse)
+                .FirstOrDefault(p =>
+                {
+                    var listener = new TcpListener(IPAddress.Loopback, p);
+                    try {
+                        listener.Start();
+                    }
+                    catch (SocketException) {
+                        return false;
+                    }
+                    finally {
+                        listener.Stop();
+                    }
+
+                    return true;
+                });
+
+            if (port < begin)
+                throw new NoPortAvailableException(
+                    $"Counld not spawn a new mongod instance. No port available within the range {begin}-{begin + count}");
+
+            lock (Sync)
+                PortsInUse.Add(port);
+
+            return port;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="port"></param>
+        public static void Free(short port)
+        {
+            lock (Sync)
+                PortsInUse.Remove(port);
+        }
+    }
+}
