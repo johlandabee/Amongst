68cb443638ecbb5e7b25b984c91ecab178dd9b5a
Steven <steven.wobser@gmail.com>
2017-06-17 21:15:25 +0200

    Added ConsoleOutputHelper

---
+++ sln/Amongst/src/Output/ConsoleOutputHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Amongst.Output
+{
+    public class ConsoleOutputHelper : IMongoDBInstanceOutputHelper
+    {
+        public void Dispose() { }
+
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+    }
+}
